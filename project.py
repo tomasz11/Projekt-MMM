921d5e5655223cd60b2df4c7a397b5ff95d13b2c
tomasz11 <tomek04k@wp.pl>
2026-06-04 00:06:09 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -5,31 +5,23 @@ from matplotlib.widgets import Slider, RadioButtons
 r2_wartosc = 10.0
 r_wartosc  = 50.0
 c_wartosc  = 0.001
-
+l_wartosc = 1
 rodzaj_sygnalu = 'trojkatny'
 amplituda = 10.0
 czestotliwosc = 10.0
-
 krok_dt = 0.0001
-czas_symulacji = 0.5
+czas_symulacji = 2
 czas = np.arange(0, czas_symulacji, krok_dt)
 
 fig, axs = plt.subplots(3, 1, figsize=(10, 8))
 plt.subplots_adjust(bottom=0.3, hspace=0.4)
-
-ax_r2    = plt.axes([0.1, 0.20, 0.35, 0.03])
-ax_r     = plt.axes([0.1, 0.15, 0.35, 0.03])
-ax_c     = plt.axes([0.1, 0.10, 0.35, 0.03])
-ax_amp   = plt.axes([0.55, 0.20, 0.35, 0.03])
-ax_freq  = plt.axes([0.55, 0.15, 0.35, 0.03])
-ax_radio = plt.axes([0.55, 0.02, 0.20, 0.10])
-
-suwak_r2 = Slider(ax_r2, 'R2 [Ohm]', 1.0, 200.0, valinit=r2_wartosc)
-suwak_r  = Slider(ax_r, 'R [Ohm]', 1.0, 200.0, valinit=r_wartosc)
-suwak_c  = Slider(ax_c, 'C [mF]', 0.1, 10.0, valinit=c_wartosc * 1000)
-suwak_amp  = Slider(ax_amp, 'Amp.', 1.0, 20.0, valinit=amplituda)
-suwak_freq = Slider(ax_freq, 'Czes.', 1.0, 500.0, valinit=czestotliwosc)
-przyciski = RadioButtons(ax_radio, ('trojkatny', 'prostokatny', 'harmoniczny'), active=0)
+suwak_r2 = Slider(plt.axes([0.1, 0.20, 0.35, 0.03]), 'R2 [Ohm]', 1.0, 200.0, valinit=r2_wartosc)
+suwak_r  = Slider(plt.axes([0.1, 0.15, 0.35, 0.03]), 'R [Ohm]', 1.0, 200.0, valinit=r_wartosc)
+suwak_c  = Slider(plt.axes([0.1, 0.10, 0.35, 0.03]), 'C [mF]', 0.1, 10.0, valinit=c_wartosc * 1000)
+suwak_l  = Slider(plt.axes([0.1, 0.05, 0.35, 0.03]), 'L [H]', 0.1, 10.0, valinit=l_wartosc)
+suwak_amp  = Slider(plt.axes([0.55, 0.20, 0.35, 0.03]), 'Amp.', 1.0, 20.0, valinit=amplituda)
+suwak_freq = Slider(plt.axes([0.55, 0.15, 0.35, 0.03]), 'Czes.', 1.0, 500.0, valinit=czestotliwosc)
+przyciski = RadioButtons(plt.axes([0.55, 0.02, 0.20, 0.10]), ('trojkatny', 'prostokatny', 'harmoniczny'), active=0)
 
 def aktualizuj(val):
     global r2_wartosc, r_wartosc, c_wartosc, amplituda, czestotliwosc, rodzaj_sygnalu
@@ -44,17 +36,41 @@ def aktualizuj(val):
     u_wejscie = np.zeros(len(czas))
     
     if rodzaj_sygnalu == 'prostokatny':
-        u_wejscie[(czas >= 0.05) & (czas <= 0.25)] = amplituda
         
-    elif rodzaj_sygnalu == 'trojkatny':
         T = 1.0 / czestotliwosc
+        u_wejscie = np.zeros(len(czas))
+
         for i in range(len(czas)):
             t_w_okresie = czas[i] % T
-            if t_w_okresie < T / 2:
-                u_wejscie[i] = -amplituda + (4 * amplituda / T) * t_w_okresie
+            if czas[i] < 0.5:
+                if t_w_okresie < T / 2:
+                    u_wejscie[i] = amplituda
+                else:
+                    u_wejscie[i] = -amplituda
             else:
-                u_wejscie[i] = 3 * amplituda - (4 * amplituda / T) * t_w_okresie
+                u_wejscie[i] = 0
+    elif rodzaj_sygnalu == 'trojkatny':
+        T = 1.0 / czestotliwosc
+        prostokatny = np.zeros(len(czas))
+
+        for i in range(len(czas)):
+            t_w_okresie = czas[i] % T
+            
+            if t_w_okresie < T / 2:
+                prostokatny[i] = amplituda
+            else:
+                prostokatny[i] = -amplituda
         
+        trojkatny = np.zeros(len(czas))
+        for i in range(1, len(czas)):
+            trojkatny[i] = trojkatny[i-1] + prostokatny[i-1] * krok_dt
+        
+        trojkatny -= np.mean(trojkatny)
+        maks = np.max(np.abs(trojkatny))
+        if maks > 0:
+            trojkatny = trojkatny * (amplituda / maks)
+
+        u_wejscie = trojkatny   
     elif rodzaj_sygnalu == 'harmoniczny':
         u_wejscie = amplituda * np.sin(2 * np.pi * czestotliwosc * czas)
         
